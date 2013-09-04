923942a568ed0b61aa163c071879d1ebde927b97
JCarles Vilaseca <joan.carles.vilaseca@gmail.com>
2013-09-04 19:19:10 +0200

    First commit

---
+++ App_Code/PriceCalc.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+
+public class PriceCalc
+{
+    public static string PriceShow(decimal price, float discount, decimal threshold)
+    {
+        if (price > threshold)
+            return "<font size=\"-2\"><strike>$" + string.Format("{0:f}",price) + "</strike> <font color=\"red\">("+ discount +"%Off)</font></font> $"+ string.Format("{0:f}",(float)price - (float)price*discount/100);
+        else
+            return "$"+string.Format("{0:f}",price);
+    }
+
+    public static string PriceValue(decimal price, float discount, decimal threshold)
+    {
+        if (price > threshold)
+            return string.Format("{0:f}",(float)price - (float)price*discount/100);
+        else
+            return string.Format("{0:f}",price);
+    }
+}
